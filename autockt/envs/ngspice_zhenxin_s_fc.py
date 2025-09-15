800fa06c5ec0f68357995d4460512e5e7c8ba765
Phuoc Pham <phuocphn@gmail.com>
2025-09-15 10:01:51 +0200

    remove adaptive reward_idx

--- autockt/envs/ngspice_zhenxin_s_fc.py
+++ autockt/envs/ngspice_zhenxin_s_fc.py
@@ -451,21 +451,6 @@ class Zhenxin_S_FC(gym.Env):
         reward = self.reward(self.cur_specs, self.specs_ideal)
         done = False
 
-        if self.env_steps < 20_000:
-            self.reward_idx = 0
-        elif self.env_steps > 20_000 and self.env_steps < 40_000:
-            self.reward_idx = 1
-        elif self.env_steps > 40_000 and self.env_steps < 60_000:
-            self.reward_idx = 2
-        elif self.env_steps > 60_000 and self.env_steps < 90_000:
-            self.reward_idx = 3
-        elif self.env_steps > 90_000 and self.env_steps < 120_000:
-            self.reward_idx = 4
-        elif self.env_steps > 120_000 and self.env_steps < 150_000:
-            self.reward_idx = 5
-        elif self.env_steps > 150_000:
-            self.reward_idx = 0
-
         # incentivize reaching goal state
         if reward >= 10:
             done = True
